0ba5a08f468ca90f59d4fc1f139748ea848cfc66
bumerc77 <bumerc.com@gmail.com>
2025-05-01 07:11:24 +0200

    board: amlogic: Bringup bananapi m5
    Change-Id: Ifa702fd005912430e66829b99f62210cdeedc14b

--- board/amlogic/configs/sm1_bananapim5_v1.h
+++ board/amlogic/configs/sm1_bananapim5_v1.h
@@ -32,7 +32,7 @@
  */
 #define CONFIG_PLATFORM_POWER_INIT
 #define CONFIG_VCCK_INIT_VOLTAGE	800		// VCCK power up voltage
-#define CONFIG_VDDEE_INIT_VOLTAGE	840		// VDDEE power up voltage
+#define CONFIG_VDDEE_INIT_VOLTAGE	850		// VDDEE power up voltage
 #define CONFIG_VDDEE_SLEEP_VOLTAGE	770		// VDDEE suspend voltage
 
 /* configs for CEC */
@@ -52,6 +52,7 @@
 #define CONFIG_BOOTLOADER_CONTROL_BLOCK
 
 #define CONFIG_CMD_BOOTCTOL_AVB
+#define CONFIG_AVB2_KPUB_VENDOR 1
 
 /* support ext4*/
 #define CONFIG_CMD_EXT4 1
@@ -165,7 +166,7 @@
             "else if test ${reboot_mode} = cold_boot; then "\
                     "setenv reboot_mode_android ""normal"";"\
                     "run storeargs;"\
-            "else if test ${reboot_mode} = fastboot -o ${reboot_mode} = bootloader; then "\
+            "else if test ${reboot_mode} = fastboot; then "\
                 "setenv reboot_mode_android ""normal"";"\
                 "run storeargs;"\
                 "fastboot;"\
@@ -667,13 +668,10 @@
 //unify build for generate encrypted bootloader "u-boot.bin.encrypt"
 #define CONFIG_AML_CRYPTO_UBOOT   1
 
-#define CONFIG_AML_SIGNED_UBOOT   0
-
 //unify build for generate encrypted kernel image
 //SRC : "board/amlogic/(board)/boot.img"
 //DST : "fip/boot.img.encrypt"
 //#define CONFIG_AML_CRYPTO_IMG       1
-#define CONFIG_SKIP_KERNEL_DTB_SECBOOT_CHECK
 
 #endif //CONFIG_AML_SECURE_UBOOT
 
@@ -692,8 +690,8 @@
 
 /* Choose One of Ethernet Type */
 #undef CONFIG_ETHERNET_NONE
-#define ETHERNET_INTERNAL_PHY
-#undef ETHERNET_EXTERNAL_PHY
+#undef ETHERNET_INTERNAL_PHY
+#define ETHERNET_EXTERNAL_PHY
 
 #define CONFIG_HIGH_TEMP_COOL 90
 #endif
